cd3e1331aa8b2d5c6e7e6625d0c91c4d3b57ef67
Елена Спиктаренко <elena.spiktarenko@gmail.com>
2023-11-17 20:41:47 +0300

    V1.0.1

--- src/MortgageCount.php
+++ src/MortgageCount.php
@@ -8,8 +8,8 @@ class MortgageCount
 {
     public function mortage($summa = 0, $years =0, $procent =0)
     {
-        if (is_null($summa) || is_null($years) || is_null($procent)){
-            return "Not enough data" ;
+        if (is_null($summa) || is_null($years) || is_null($procent) || is_numeric($summa) || is_numeric($years) || is_numeric($procent) ){
+            return "All data must be exists and numeric " ;
         }
         else{
             $var = $summa*$years*$procent/100;
